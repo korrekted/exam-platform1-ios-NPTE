cac0dbc8cb37891c2dd053a505706cc5ec6ce92d
Sergei Pankov <sergeipankov@Sergeis-MacBook-Air.local>
2022-04-14 13:01:47 +0100

    config

--- PTA/Classes/Service/Stats/Share/StatsShareManager.swift
+++ PTA/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.pta.widget")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.npteptpta.widget")
     }
     
     @discardableResult

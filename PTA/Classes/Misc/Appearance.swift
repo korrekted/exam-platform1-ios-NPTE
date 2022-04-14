290698f8fc2463486ae98ea7432724e1e4e3fefa
akonst17 <akonst17@gmail.com>
2022-04-14 12:28:17 +0300

    upgrade; build 11

--- PTA/Classes/Misc/Appearance.swift
+++ PTA/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 91, green: 74, blue: 164)
     static let progress2Color = UIColor(integralRed: 255, green: 189, blue: 148)
     static let progress3Color = UIColor(integralRed: 182, green: 144, blue: 235)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- PTA/Classes/Service/Stats/Share/StatsShareManager.swift
+++ PTA/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.pta.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.pta.widget")
     }
     
     @discardableResult

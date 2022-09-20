c59832966aa115df97e352fec741ba4deed0e794
akonst17 <akonst17@gmail.com>
2022-09-20 11:45:44 +0300

    интеграция sdk 2.0; новые урлы; build 16

--- PTA/Classes/Misc/GlobalDefinitions.swift
+++ PTA/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "x87XKmFOIUtJtIeZ"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "mgjavDcs96DjENkJ"
 
     static let applicationTag = "EXAM_NPTE_PT_PTA"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "mgjavDcs96DjENkJ"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "VxZacG48a2WZlqPI"
 
     static let appStoreUrl = "https://apps.apple.com/app/npte/id1569397550"
     static let appleAppID = "1569397550"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
